3bd4e9d19575b476530d9f4a8c1854e058a6890a
Wojciech <Wojciech@192.168.1.14>
2020-04-24 21:51:43 +0200

    Parametr specyfikacji

---
+++ src/Parameter.java
@@ -0,0 +1,16 @@
+
+public class Parameter {
+	private String name;
+	private String requirements;
+	private String unit;
+	
+	public Parameter(String name, String requirements, String unit){
+		this.name = name;
+		this.requirements = requirements;
+		this.unit = unit;
+	}
+	
+	public String toString(){
+		return this.name + ", " + this.requirements + ", " + this.unit;
+	}
+}
--- src/Specification.java
+++ src/Specification.java
@@ -1,9 +1,15 @@
 
 public class Specification {
-	
 	private String number;
 	private String name;
 	
-	kkk
-
+	public Specification(String number, String name){
+		this.number = number;
+		this.name = name;
+	}
+	
+	public String toString(){
+		return this.number + " " + this.name;
+	}
+	
 }
